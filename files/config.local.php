ab82e8d008ccaa84840b331cbb51be2a884b32b8
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-02-12 18:47:56 +0100

    restructure code; add some categories

--- files/config.local.php
+++ files/config.local.php
@@ -3,8 +3,48 @@
   $CONFIG['socket'] = 'unix:///var/run/collectd-socket';
   $CONFIG['graph_type'] = 'hybrid';
 
-  $CONFIG['cat']['Server und Gateways'] = array('segen-router', 'servnix42', 'druschba');
-  $CONFIG['cat']['Zoofenster'] = array('zoofenster22', 'zoofenster24', 'zoofenster27', 'zoofenster_33_2');
-  $CONFIG['cat']['Segenkirche'] = array('segen-no-2', 'segen-no-5', 'segen-so-2', 'segen-so-5', 'segen-sw-2', 'segen-sw-5', 'segen-nw-2', 'segen-nw-5', 'segen-top-sw');
+  $CONFIG['cat']['berlin.freifunk.net'] = array(
+    'vpn03.berlin.freifunk.net',
+    'firmware.berlin.freifunk.net',
+    'vmhost03.berlin.freifunk.net',
+    'monitor.berlin.freifunk.net',
+    'bgp.berlin.freifunk.net'
+  );
+  $CONFIG['cat']['Server und Gateways'] = array(
+    'segen-router', 'servnix42', 'druschba'
+  );
+  $CONFIG['cat']['Zoofenster'] = array('
+    zoofenster22', 'zoofenster24', 'zoofenster27', 'zoofenster_33_2'
+  );
+  $CONFIG['cat']['Segenkirche'] = array(
+    'segen-no-2', 'segen-no-5', 'segen-so-2', 'segen-so-5', 'segen-sw-2',
+    'segen-sw-5', 'segen-nw-2', 'segen-nw-5', 'segen-top-sw'
+  );
   $CONFIG['cat']['Eliaskirche'] = array('Eliaskirche-West', 'eliaskircheS5G');
+  $CONFIG['cat']['Rathaus Kreuzberg'] = array(
+    'rhxb-2-no',
+    'rhxb-2-nw',
+    'rhxb-2-so',
+    'rhxb-2.sw',
+    'rhxb-rt1',
+    'rhxb-warte'
+  );
+  $CONFIG['cat']['E-Lok'] = array(
+    'elok-lascafe',
+    'elok-medienraum',
+    'elok-nordwest'
+  );
+  $CONFIG['cat']['Zwingli'] = array(
+    'Zwingli-Core',
+    'Zwingli-Ost-2GHz'
+  );
+  $CONFIG['cat']['Finowst. 2a'] = array(
+    'f2a-core-rt',
+    'f2a-omni-2ghz',
+    'f2a-sued'
+  );
+  $CONFIG['cat']['Neukoelln Arcaden'] = array(
+    'arcaden-klunkerkranich',
+    'arcaden-rhxb-kindl'
+  );
 ?>
