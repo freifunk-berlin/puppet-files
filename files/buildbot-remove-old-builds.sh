eef7a9c0028e7aa6561c2eace3286f3a5bfd7fa8
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-10-27 11:51:29 +0100

    add cron script for buildbot
    The script should cleanup old builds from the unstable firmware directories.

---
+++ files/buildbot-remove-old-builds.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /usr/local/src/www/htdocs/buildbot/unstable/ar71xx && rm -fr $(ls -t | tail -n +20)
+cd /usr/local/src/www/htdocs/buildbot/unstable/ar71xx_mikrotik && rm -fr $(ls -t | tail -n +20)
+cd /usr/local/src/www/htdocs/buildbot/unstable/x86 && rm -fr $(ls -t | tail -n +20)
+cd /usr/local/src/www/htdocs/buildbot/unstable/mpc85xx && rm -fr $(ls -t | tail -n +20)
