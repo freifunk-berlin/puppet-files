06dc7a441ef3cb6eef5592864a7764bd61d1e252
sarumpaet <sarumpaet@users.noreply.github.com>
2015-11-05 21:25:39 +0100

    buildbot-remove-old-builds.sh: add ramips

--- files/buildbot-remove-old-builds.sh
+++ files/buildbot-remove-old-builds.sh
@@ -4,3 +4,4 @@ cd /usr/local/src/www/htdocs/buildbot/unstable/ar71xx && rm -fr $(ls -t | tail -
 cd /usr/local/src/www/htdocs/buildbot/unstable/ar71xx_mikrotik && rm -fr $(ls -t | tail -n +20)
 cd /usr/local/src/www/htdocs/buildbot/unstable/x86 && rm -fr $(ls -t | tail -n +20)
 cd /usr/local/src/www/htdocs/buildbot/unstable/mpc85xx && rm -fr $(ls -t | tail -n +20)
+cd /usr/local/src/www/htdocs/buildbot/unstable/ramips && rm -fr $(ls -t | tail -n +20)
