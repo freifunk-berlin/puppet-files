335ae6286617e7875248e3d9fdebb1db87a7d0ff
Sven Roederer <devel-sven@geroedel.de>
2017-09-03 19:35:57 +0200

    tunnel-berlin: add up/down-script
    to manage the firewall / NAT
    * detects the interface for NATing clients by looking up the default-route

---
+++ files/tunnel-berlin/community-berlin_updown.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+log() {
+  logger -t community-tunnel_openvpn $1
+  echo >&2 $1
+}
+
+up_actions() {
+        log "running up-actions"
+        UPLINK_IF=`ip route |grep default|awk '{print $5}'`
+        log "uplink interface: $UPLINK_IF"
+        # create table for our tunnel
+        # ignore error when already there
+        iptables -N community-tunnel 2>/dev/null
+        # check for rule managing out forward-traffic
+        # and install
+        log "checking for / setup of MASQUERADING"
+        iptables -t nat -C POSTROUTING -s "$TUNNEL_IP/$TUNNEL_MASK" -o $UPLINK_IF -j MASQUERADE 2>/dev/null || \
+                iptables -t nat -I POSTROUTING -s $TUNNEL_IP/$TUNNEL_MASK -o $UPLINK_IF -j MASQUERADE 2>/dev/null
+        log "checking for / setup of rules for tunnel-clients"
+        iptables -C INPUT -i $TUNNEL_IF -j community-tunnel || \
+                iptables -I INPUT -i $TUNNEL_IF -j community-tunnel
+        iptables -C FORWARD -i $TUNNEL_IF -j community-tunnel || \
+                iptables -I FORWARD -i $TUNNEL_IF -j community-tunnel
+        iptables -F community-tunnel
+        iptables -A community-tunnel -o $UPLINK_IF -j ACCEPT
+        iptables -A community-tunnel -j REJECT --reject-with net-prohib
+}
+
+down_actions() {
+        log "running down-actions"
+        log "stopping all FORWARDING from tunnel"
+        iptables -F community-tunnel
+        iptables -A community-tunnel -j REJECT --reject-with net-prohib
+}
+
+if [ $# -eq 0 ]; then
+  log "too few number of arguments"
+  exit 1
+fi
+
+if [ $1 = "up" ]; then
+        TUNNEL_IF=$2
+        TUNNEL_IP=$5
+        TUNNEL_MASK=$6
+        up_actions
+elif [ $1 = "down" ]; then
+  down_actions
+else
+  log "bad parameter: $1"
+fi
+
